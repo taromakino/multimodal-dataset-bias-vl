5192058070db114bab11f1056031839a2dea7214
Taro Makino <taro@nyu.edu>
2023-01-27 11:53:17 -0500

    Minor changes

--- multimodal_dataset_bias_vqa/run.py
+++ multimodal_dataset_bias_vqa/run.py
@@ -15,29 +15,29 @@ resource.setrlimit(resource.RLIMIT_NOFILE, (20480, rlimit[1]))
 
 
 @ex.automain
-def main(config):
-    config = copy.deepcopy(config)
-    pl.seed_everything(config["seed"])
+def main(_config):
+    _config = copy.deepcopy(_config)
+    pl.seed_everything(_config["seed"])
 
-    dm = MTDataModule(config, dist=True)
-    model = FIBERTransformerSS(config)
+    dm = MTDataModule(_config, dist=True)
+    model = FIBERTransformerSS(_config)
 
-    os.makedirs(config["log_dir"], exist_ok=True)
+    os.makedirs(_config["log_dir"], exist_ok=True)
     checkpoint_callback = pl.callbacks.ModelCheckpoint(
         save_top_k=1,
         verbose=True,
         monitor="val_loss",
-        mode=config["val_mode"],
+        mode=_config["val_mode"],
         save_last=True,
         filename="best"
     )
     callbacks = [checkpoint_callback]
 
-    task = f'{config["task"]}'
+    task = f'{_config["task"]}'
     logger = pl.loggers.CSVLogger(
-        config["log_dir"],
+        _config["log_dir"],
         name="",
-        version=config["seed"],
+        version=_config["seed"],
     )
 
     model.freeze()
@@ -46,26 +46,26 @@ def main(config):
     elif task == "posterior_kld":
         model.vqa_classifier.encoder_x.requires_grad_(True)
 
-    num_gpus = config["num_gpus"] if isinstance(config["num_gpus"], int) else len(config["num_gpus"])
-    grad_steps = max(config["batch_size"] // (config["per_gpu_batchsize"] * num_gpus * config["num_nodes"]), 1)
+    num_gpus = _config["num_gpus"] if isinstance(_config["num_gpus"], int) else len(_config["num_gpus"])
+    grad_steps = max(_config["batch_size"] // (_config["per_gpu_batchsize"] * num_gpus * _config["num_nodes"]), 1)
 
     trainer = pl.Trainer(
-        gpus=config["num_gpus"],
-        num_nodes=config["num_nodes"],
-        precision=config["precision"],
+        gpus=_config["num_gpus"],
+        num_nodes=_config["num_nodes"],
+        precision=_config["precision"],
         accelerator="ddp",
         benchmark=True,
         deterministic=True,
-        max_epochs=config["max_epoch"],
+        max_epochs=_config["max_epoch"],
         callbacks=callbacks,
         logger=logger,
         prepare_data_per_node=False,
         replace_sampler_ddp=False,
         accumulate_grad_batches=grad_steps,
-        resume_from_checkpoint=config["resume_from"], # Load everything (model weights, optimizer, lr scheduler, etc)
+        resume_from_checkpoint=_config["resume_from"], # Load everything (model weights, optimizer, lr scheduler, etc)
     )
 
-    if not config["test_only"]:
+    if not _config["test_only"]:
         trainer.fit(model, datamodule=dm)
     else:
         trainer.test(model, datamodule=dm)
