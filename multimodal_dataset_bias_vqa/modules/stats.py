173adaa910a70e91f2c2983f08a3508e85f052a8
Taro Makino <taro@nyu.edu>
2023-03-06 15:38:40 -0500

    Minor changes

--- multimodal_dataset_bias_vqa/modules/stats.py
+++ multimodal_dataset_bias_vqa/modules/stats.py
@@ -4,6 +4,6 @@ import torch
 EPSILON = 1e-8
 
 
-def diag_gaussian_log_prob(x, mu, var, device):
-    c = 2 * torch.pi * torch.ones(1).to(device)
+def diag_gaussian_log_prob(x, mu, var):
+    c = 2 * torch.pi * torch.ones(1).to(x.get_device())
     return (-0.5 * (torch.log(c) + var.log() + (x - mu).pow(2).div(var))).sum(-1)
