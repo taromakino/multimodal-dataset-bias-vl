49201c348114a4b40fbf90488afff1d61c665458
Taro Makino <taro@nyu.edu>
2023-05-28 11:43:42 -0700

    Fix multimodal classifier input dim

--- multimodal_dataset_bias_vl/modules/classify.py
+++ multimodal_dataset_bias_vl/modules/classify.py
@@ -8,7 +8,7 @@ class MultimodalClassifier(nn.Module):
     def __init__(self, input_dim, hidden_dims, output_dim, loss_fn):
         super().__init__()
         self.loss_fn = loss_fn
-        self.model = MLP(2 * input_dim, hidden_dims, output_dim)
+        self.model = MLP(input_dim, hidden_dims, output_dim)
 
 
     def forward(self, x, y_true):
