23bdbfcdf18958c50c18188ec96c9bb86f346088
Taro Makino <taro@nyu.edu>
2023-05-29 07:56:10 -0700

    Minor bug fix in unimodal classifier

--- multimodal_dataset_bias_vl/modules/classify.py
+++ multimodal_dataset_bias_vl/modules/classify.py
@@ -32,7 +32,7 @@ class UnimodalClassifier(nn.Module):
         prob_image = torch.sigmoid(self.image_model(x_image))
         prob_text = torch.sigmoid(self.text_model(x_text))
         y_pred = torch.log((prob_image + prob_text) / 2)
-        loss = self.loss(y_pred, y_true).mean()
+        loss = self.loss_fn(y_pred, y_true).mean()
         return {
             "loss": loss,
             "logits": y_pred.log()
